c47e654bd9ed8010b4305a18876daf14cd229006
ryan <ryanennns@gmail.com>
2025-05-19 16:41:25 -0400

    basics

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,54 @@
 import express from "express";
 import cors from "cors";
 import multer from "multer";
+import * as fs from "node:fs";
+import {FLASHED_REGEX, KILL_FEED_REGEX, THREW_REGEX} from "./services/const";
 
 const app = express();
+const upload = multer({dest: 'uploads/'});
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send({
-    message: "Hello from the server!",
-  });
+    res.send({
+        message: "Hello from the server!",
+    });
 });
 
+
+app.post('/upload', upload.single('logFile'), (req, res) => {
+    const path = req?.file?.path ?? '';
+    const rawLog = fs.readFileSync(path, 'utf-8');
+
+    const parsedData = parseLog(rawLog);
+
+    console.log(parsedData);
+
+    fs.unlinkSync(path);
+    res.json(parsedData);
+});
+
+function parseLog(text: string) {
+    const kills = text.match(KILL_FEED_REGEX) ?? [];
+    const flashes = text.match(FLASHED_REGEX) ?? [];
+    const throws = text.match(THREW_REGEX) ?? [];
+
+    return {
+        totalLines: text.split('\n').length,
+        rounds: (text.match(/Round_Start/g) || []).length,
+        kills: (kills || []).length,
+        flashes: (flashes || []).length,
+        throws: {
+            total: throws.length,
+            smokegrenade: throws.filter(t => t.includes('smokegrenade')).length,
+            flashbang: throws.filter(t => t.includes('flashbang')).length,
+            hegrenade: throws.filter(t => t.includes('hegrenade')).length,
+            molotov: throws.filter(t => t.includes('molotov')).length,
+        },
+    };
+}
+
+
 app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
+    console.log("Server running on http://localhost:3001");
 });
---
+++ src/services/const.ts
@@ -0,0 +1,3 @@
+export const KILL_FEED_REGEX = /^.*] killed ".*$/gm;
+export const FLASHED_REGEX = /^.*>" blinded for .*$/gm;
+export const THREW_REGEX = /^.*>" threw .*$/gm;
